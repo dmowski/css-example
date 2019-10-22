54333c7435c807a323294ee49a146654da298ca4
Alexandr Dmowski <dmowski@yandex.ru>
2019-10-22 19:32:36 +0300

    Don't show popup. Run refresh immediately if click to extension icon

--- extension/popup.js
+++ extension/popup.js
@@ -2,20 +2,23 @@
   const updateMessage = {
     update: true,
   };
-
+  const refreshActionHandler = () => {
+    console.log("Popup send message", updateMessage);
+    chrome.tabs.query(
+      {
+        active: true,
+        currentWindow: true,
+      },
+      tabs => {
+        chrome.tabs.sendMessage(tabs[0].id, updateMessage);
+        window.close();
+      }
+    );
+  };
   window.addEventListener("load", () => {
     const button = document.getElementById("refreshButton");
-    button.addEventListener("click", () => {
-      console.log("Popup send message", updateMessage);
-      chrome.tabs.query(
-        {
-          active: true,
-          currentWindow: true,
-        },
-        tabs => {
-          chrome.tabs.sendMessage(tabs[0].id, updateMessage);
-        }
-      );
-    });
+    button.addEventListener("click", refreshActionHandler);
+
+    refreshActionHandler();
   });
 })();
