bf8c66e52efe4a35024bc196133f0000640b60ff
Alexandr Dmowski <dmowski@yandex.ru>
2019-11-02 21:23:05 +0300

    Send stop/start event from popup to background page
    Enable disable buttons if necessary (create listener for change info about status)

--- extension/popup.js
+++ extension/popup.js
@@ -1,23 +1,50 @@
 (function() {
-  const updateMessage = {
-    update: true,
-  };
-  const refreshActionHandler = () => {
+  const sendMessage = message => {
     chrome.tabs.query(
       {
         active: true,
         currentWindow: true,
       },
       tabs => {
-        chrome.tabs.sendMessage(tabs[0].id, updateMessage);
-        window.close();
+        chrome.tabs.sendMessage(tabs[0].id, message);
       }
     );
   };
-  window.addEventListener("load", () => {
-    const button = document.getElementById("refreshButton");
-    button.addEventListener("click", refreshActionHandler);
 
-    refreshActionHandler();
+  chrome.runtime.onMessage.addListener(
+    async (request, sender, sendResponce) => {
+      if (!sender.tab || sender.tab.active !== true) {
+        return;
+      }
+      const startWatchButton = document.getElementById("start-watch");
+      const stopWatchButton = document.getElementById("stop-watch");
+      if (request.watchStatus === true) {
+        startWatchButton.style.display = "none";
+        stopWatchButton.style.display = "block";
+      } else {
+        startWatchButton.style.display = "block";
+        stopWatchButton.style.display = "none";
+      }
+    }
+  );
+
+  window.addEventListener("load", () => {
+    const startWatchButton = document.getElementById("start-watch");
+    startWatchButton.addEventListener("click", () => {
+      sendMessage({
+        watch: true,
+      });
+    });
+
+    const stopWatchButton = document.getElementById("stop-watch");
+    stopWatchButton.addEventListener("click", () => {
+      sendMessage({
+        watch: false,
+      });
+    });
+
+    sendMessage({
+      getInfo: true,
+    });
   });
 })();
